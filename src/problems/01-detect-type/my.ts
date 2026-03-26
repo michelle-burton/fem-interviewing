7b92d46f4138d3262f4611abe28881f90b467434
Michelle Burton <msmichelleburton@yahoo.com>
2026-03-25 19:25:10 -0500

    added the function

--- src/problems/01-detect-type/my.ts
+++ src/problems/01-detect-type/my.ts
@@ -26,3 +26,9 @@ type DetectType =
 //   }
 //   return (Object.getPrototypeOf(value)?.constructor?.name ?? 'object').toLowerCase()
 // }
+
+export function detectType(value: any): DetectType {
+  if (value == null) {
+    return `${value}`
+  }
+}
