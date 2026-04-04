02c6778ca60c3ebe52372d146e37b04299d22f6e
Michelle Burton <msmichelleburton@yahoo.com>
2026-04-04 15:13:49 -0500

    tesing funtions taking funtions

---
+++ src/_test/scripts/script.js
@@ -0,0 +1,33 @@
+console.log('JS file loaded')
+const output = document.querySelector('#output')
+const bt = document.querySelector('#runBt')
+
+const users = [
+  { name: 'Alice', age: 22, active: true },
+  { name: 'Bob', age: 17, active: true },
+  { name: 'Charlie', age: 25, active: false },
+  { name: 'Diana', age: 30, active: true },
+]
+
+function Hello(x) {
+  console.log('in Hello ' + x)
+}
+
+Hello('michelle')
+
+function names(n) {
+  return 'Alice'
+}
+// ["Alice", "Diana"]
+console.log(names(users))
+
+const runMeBT = () => {
+  console.log('in the function')
+}
+
+function run(fn) {
+  console.log('before running')
+  fn('michelle in the fn')
+}
+
+run(Hello)
