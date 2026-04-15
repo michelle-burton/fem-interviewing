37ced6ff8e083da56ec99f81576af68ea82e6c5d
Michelle Burton <msmichelleburton@yahoo.com>
2026-04-15 07:00:01 -0500

    add items

--- src/_test/scripts/testing.js
+++ src/_test/scripts/testing.js
@@ -1,10 +1,41 @@
 console.log('testing JS loaded!')
 
-const myArr = [1, 2, 3, 4, 5]
+const myArr = [1, 2, 2, 3, 1]
+// one
+function doubleNumbers(arr) {
+  return arr.map((num) => num * 2)
+}
+doubleNumbers(myArr)
 
+// two
 function processNumbers(arr) {
-    return arr.filter(num => (num % 2 === 0))
-        .map(num => num * 2)
+  return arr.filter((item) => item > 3).map((item) => item * 2)
 }
 
 processNumbers(myArr)
+
+// Count how many numbers are greater than 3
+function countGreaterThanThree(arr) {
+  const result = arr.reduce((accum, item, i, arr) => {
+    if (item > 3) {
+      accum += 1
+    }
+    accum
+  }, 0)
+  return SpeechRecognitionResult
+}
+
+// tighten up
+function countGreaterThanThree(arr) {
+    return arr.reduce(
+        (accum, item) => {
+      item > 3 ? accum += 1 : accum,
+  }, 0)
+}
+
+countGreaterThanThree(myArr)
+
+
+function sumNumbers(arr) {
+    return arr.reduce((acc, item) =>  acc + item, 0)
+}
