fb4d89af28a7f4d40308a836701ba889cf6722f8
Michelle Burton <msmichelleburton@yahoo.com>
2026-04-16 21:07:16 -0500

    add

--- src/_test/scripts/testing.js
+++ src/_test/scripts/testing.js
@@ -1,6 +1,6 @@
 console.log('testing JS loaded!')
 
-const myArr = [1, 2, 2, 3, 1]
+const myArr = [1, 2, 3]
 // one
 function doubleNumbers(arr) {
   return arr.map((item) => item * 2)
@@ -24,20 +24,58 @@ function sumNumbers(arr) {
   return arr.reduce((accum, item) => accum + item, 0)
 }
 
-const arr = [1, 2, 2, 3, 1]
-const unique = [...new Set(arr)]
+const array = [1, 2, 2, 3, 1]
+const unique = [...new Set(array)]
 
-console.log(unique)
-console.log(typeof unique)
-console.log(Array.isArray(unique))
-console.log(typeof new Set([1, 2, 3]))
-
+// console.log(unique)
+// console.log(typeof unique)
+// console.log(Array.isArray(unique))
+// console.log(typeof new Set([1, 2, 3]))
 
 function run() {
-    return addTen;
+  return addTen
 }
 
 function addTen(x) {
-    return x + 10;
+  return x + 10
 }
 
+function addFive(arr) {
+  return arr.map((num) => num + 5)
+}
+
+function getEvens(arr) {
+  return arr.filter((num) => num % 2 === 0)
+}
+
+function sum(arr) {
+  return arr.reduce((acc, num) => {
+    return acc + num
+  }, 0)
+}
+
+function evenDoubles(arr) {
+  return arr.filter((num) => num % 2 === 0).map((num) => num * 2)
+}
+
+const users = [
+  { name: 'Alice', active: true },
+  { name: 'Bob', active: false },
+  { name: 'Carol', active: true },
+]
+console.table(users)
+// IDK --
+function getActiveNames(users) {
+
+  const users2 = [...new Set(users)]
+  //console.log(type(users2))
+  //console.table(users2)
+  //console.log('activeUsers')
+   return users2.filter((users) => users.active === true)
+    .map(user => user.name)
+
+   // console.table(activeUsers)
+    
+}
+
+getActiveNames(users)
